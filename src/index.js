9f103fcfc83e943ac608fdfef8820bd996fab62f
phandung031020 <66107363+phandung031020@users.noreply.github.com>
2022-07-14 09:41:46 +0700

    test 1

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ const port = 3000;
 
 const route = require('./routes');
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(
-    express.urlencoded({
-        extended: true,
-    }),
+                    app.use(express.static(path.join(__dirname, 'public')));
+                    app.use(
+                        express.urlencoded({
+                            extended: true,
+                        }),
 );
 
 app.use(express.json());
